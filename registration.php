2e1f359d5e09f5aea7598a34da4d108683603e64
Roman Glushko <weysman@atwix.com>
2017-12-13 15:27:00 +0200

    Inited the module

---
+++ registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author Atwix Team
+ * @copyright Copyright (c) 2017 Atwix (https://www.atwix.com/)
+ * @package Atwix_FixDefaultShippingTracking
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Atwix_FixDefaultShippingTracking',
+    __DIR__
+);
